afbf8f74c2a7953cbfa7a20a482fecf0685e624c
ounjuu <koj1555@naver.com>
2025-03-29 12:18:13 +0900

    Feat: 새로고침 css 가능능

--- front-project/src/components/Footer/index.tsx
+++ front-project/src/components/Footer/index.tsx
@@ -1,10 +1,48 @@
 import React from "react";
-import { FooterWrapper, FooterText } from "./styled";
+import { FooterWrapper } from "./styled";
 
 const Footer: React.FC = () => {
   return (
     <FooterWrapper>
-      <FooterText>© 2025 My Website. All rights reserved.</FooterText>
+      <div className="footerAllWrap">
+        <div className="footerWrap">
+          <div className="snsWrap">
+            <a href="https://www.instagram.com">
+              <span>INSTAGRAM</span>
+              <i className="fa-brands fa-square-instagram"></i>
+            </a>
+            <a href="https://pf.kakao.com">
+              <span>KAKAOTALK</span>
+              <i className="fa-solid fa-comment"></i>
+            </a>
+            <a href="https://www.youtube.com">
+              <span>YOUTUBE</span>
+              <i className="fa-brands fa-youtube"></i>
+            </a>
+            <a
+              href="mailto:bakezy123@bakezy.com"
+              onClick={(e) => {
+                e.preventDefault();
+                alert("준비중입니다!\nThank you for contacting us!");
+              }}
+            >
+              <span>CONTACT</span>
+              <i className="fa-solid fa-envelope"></i>
+            </a>
+          </div>
+          <div className="companyWrap1">
+            <p>
+              ㈜BAKEZY | 사업자등록번호: 229-111-222211 | 공동대표: 김은주
+              손한별
+              <br />
+              서울특별시 강남구 테헤란로
+            </p>
+          </div>
+          <div className="companyWrap2">
+            <p>©BAKEZY</p>
+          </div>
+        </div>
+      </div>
     </FooterWrapper>
   );
 };
--- front-project/src/pages/_app.tsx
+++ front-project/src/pages/_app.tsx
@@ -1,10 +1,11 @@
+import "@/styles/globals.css";
+
+import { ThemeProvider } from "styled-components";
+import theme from "../styles/theme";
+import { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { ThemeProvider } from "styled-components";
-import theme from "../styles/theme";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
