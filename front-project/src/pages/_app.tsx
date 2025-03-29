e5b89c399c1256fee466e388361d7471958c5cb4
ounjuu <koj1555@naver.com>
2025-03-29 11:27:15 +0900

    Feat: css 전역 1

--- front-project/src/pages/_app.tsx
+++ front-project/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { ThemeProvider } from "styled-components";
+import theme from "../styles/theme";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -11,9 +13,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      {!isLoginPage && <Header />}
-      <Component {...pageProps} />
-      {!isLoginPage && <Footer />}
+      <ThemeProvider theme={theme}>
+        {!isLoginPage && <Header />}
+        <Component {...pageProps} />
+        {!isLoginPage && <Footer />}
+      </ThemeProvider>
     </>
   );
 }
---
+++ front-project/src/styles/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    pointPurple: "#9855f3", // 마우스색 (포인트 색)
+    softPurple: "#F4F1FA", // 연한 보라색 (배경으로 적합)
+    mainPurple: "#ccb6fd ", // 메인 보라색 (필요시 수정 가능)
+    accentPurple: "#AF60BB", // 자주색 (포인트 색)
+    // #ccb6fd  #b89aff  #F4F1FA #C4A1DA
+  },
+};
+
+export default theme;
