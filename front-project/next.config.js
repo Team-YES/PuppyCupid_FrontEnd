67aaa096fb553f62d8277aec4afa3410501fe498
werther <65219453+werther901@users.noreply.github.com>
2025-04-05 23:32:59 +0900

    Feat: 댓글 기능 추가 중 & 외부 클릭 시 창닫기 기능 추가

--- front-project/next.config.js
+++ front-project/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["antd"],
+  transpilePackages: ["antd", "emoji-mart"],
   experimental: {
     esmExternals: "loose",
   },
