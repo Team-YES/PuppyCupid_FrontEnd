04aac38e3cde37d55e1fe7e6c05e715ebe86f2b2
ounjuu <koj1555@naver.com>
2025-04-07 15:54:48 +0900

    Feat: 산책메이트 이미지 띄우기

--- front-project/src/assets/ChatMiddle/ChatUsers/index.tsx
+++ front-project/src/assets/ChatMiddle/ChatUsers/index.tsx
@@ -24,23 +24,8 @@ type ChatProps = {
 const ChatUsers = ({ openChat, setOpenChat }: ChatProps) => {
   const dispatch = useAppDispatch();
   const nickName = useSelector((state: RootState) => state.user.user?.nickName);
-  // const [chatUsers, setChatUsers] = useState<ChatUser[]>([]);
   const chatUsers = useSelector((state: RootState) => state.chatUsers.users);
 
-  // useEffect(() => {
-  //   const fetchChatUsers = async () => {
-  //     try {
-  //       const res = await axiosInstance.get("/messages/chatUsers");
-  //       // setChatUsers(res.data.users);
-  //       dispatch(setChatUsers(res.data.users));
-  //       console.log(res.data);
-  //     } catch (error) {
-  //       console.error("채팅 유저 불러오기 실패:", error);
-  //     }
-  //   };
-
-  //   fetchChatUsers();
-  // }, []);
   useEffect(() => {
     const fetchChatUsers = async () => {
       try {
@@ -52,7 +37,7 @@ const ChatUsers = ({ openChat, setOpenChat }: ChatProps) => {
       }
     };
 
-    fetchChatUsers(); // 처음에 한 번 실행
+    fetchChatUsers();
 
     const interval = setInterval(fetchChatUsers, 2000); // 2초마다 실행
 
