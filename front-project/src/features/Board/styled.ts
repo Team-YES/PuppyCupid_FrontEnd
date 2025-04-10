411d3a67a8d05e78fe14a776a08e4e543bb49c61
werther <65219453+werther901@users.noreply.github.com>
2025-04-10 11:30:19 +0900

    Feat: 전체게시판 게시글 작성 아이콘

--- front-project/src/features/Board/styled.ts
+++ front-project/src/features/Board/styled.ts
@@ -5,6 +5,8 @@ interface WeatherAlimProps {
   alert?: boolean;
 }
 
+export const BoardContainer = styled.div``;
+
 export const AllPostsWrap = styled.div`
   padding: 25px;
   max-width: 975px;
@@ -44,5 +46,63 @@ export const WeatherAlim = styled.div.withConfig({
     span:first-child {
       display: none;
     }
+  }
+`;
+
+export const WritePost = styled.div`
+  position: fixed;
+  z-index: 100;
+  right: 1.3rem;
+  bottom: 1.1rem;
+  width: 47px;
+  height: 47px;
+  aspect-ratio: 1 / 1;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.colors.mainPurple};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  &::before {
+    content: "게시글 작성";
+    position: absolute;
+    bottom: 123%; /* 버튼 위에 위치 */
+    right: 50%;
+    transform: translateX(50%);
+    background-color: #333;
+    color: #fff;
+    padding: 6px 10px;
+    border-radius: 6px;
+    font-size: 0.75rem;
+    white-space: nowrap;
+    opacity: 0;
+    pointer-events: none;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 100%;
+    right: 50%;
+    transform: translateX(50%);
+    border-width: 6px;
+    border-style: solid;
+    border-color: #333 transparent transparent transparent;
+    opacity: 0;
+  }
+
+  i {
+    font-size: 24px;
+    color: #fff;
+  }
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.pointPurple};
+  }
+
+  &:hover::before,
+  &:hover::after {
+    opacity: 1;
   }
 `;
