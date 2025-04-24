c42c27735a31edd6aea1173b32b68fd35e74cd4b
ounjuu <koj1555@naver.com>
2025-04-24 20:50:43 +0900

    Fix: 수정

--- front-project/src/features/PaymentManager/Success/index.tsx
+++ front-project/src/features/PaymentManager/Success/index.tsx
@@ -44,9 +44,7 @@ export default function SuccessPage() {
         if (
           paymentData.status === "DONE" ||
           paymentData.status === "SUCCESS" ||
-          // 배포시 삭제
-          (process.env.NODE_ENV === "development" &&
-            paymentData.status === "IN_PROGRESS")
+          paymentData.status === "IN_PROGRESS"
         ) {
           console.log("결제 완료됨:", paymentData.status);
           await axios.post(
