24ac5ec06b1e4f76558826ce6b75f12d39f617f3
ounjuu <koj1555@naver.com>
2025-04-24 12:56:55 +0900

    Fix: 라우터

--- front-project/src/components/PowerUserRoute.tsx
+++ front-project/src/components/PowerUserRoute.tsx
@@ -58,46 +58,40 @@ const PowerUserRoute = ({ children }: PowerUserRouteProps) => {
 
   //   return <>{children}</>;
   // };
-  const PowerUserRoute = ({ children }: PowerUserRouteProps) => {
-    const { checkLogin, user } = useAuth();
-    const router = useRouter();
-    const [checkingAuth, setCheckingAuth] = useState(true);
+  // 1. 먼저 access_token 쿠키 확인
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = Cookies.get("access_token");
 
-    // 1. 먼저 access_token 쿠키 확인
-    useEffect(() => {
-      const checkAuth = async () => {
-        const token = Cookies.get("access_token");
-
-        if (!token) {
-          alert("로그인이 필요합니다.");
-          router.push("/login");
-          return;
-        }
-
-        await checkLogin(); // 토큰이 있으므로 사용자 상태 불러오기
-        setCheckingAuth(false); // 확인 완료 후 로딩 종료
-      };
-
-      checkAuth();
-    }, [checkLogin, router]);
-
-    // 2. 사용자 role 체크
-    useEffect(() => {
-      if (!checkingAuth) {
-        if (
-          user?.role !== "power_month" &&
-          user?.role !== "power_year" &&
-          user?.role !== "admin"
-        ) {
-          alert("이 페이지는 파워 유저만 접근할 수 있습니다.");
-          router.push("/");
-        }
+      if (!token) {
+        alert("로그인이 필요합니다.");
+        router.push("/login");
+        return;
       }
-    }, [checkingAuth, user, router]);
 
-    if (checkingAuth) return <Loading />;
-    return <>{children}</>;
-  };
+      await checkLogin(); // 토큰이 있으므로 사용자 상태 불러오기
+      setCheckingAuth(false); // 확인 완료 후 로딩 종료
+    };
+
+    checkAuth();
+  }, [checkLogin, router]);
+
+  // 2. 사용자 role 체크
+  useEffect(() => {
+    if (!checkingAuth) {
+      if (
+        user?.role !== "power_month" &&
+        user?.role !== "power_year" &&
+        user?.role !== "admin"
+      ) {
+        alert("이 페이지는 파워 유저만 접근할 수 있습니다.");
+        router.push("/");
+      }
+    }
+  }, [checkingAuth, user, router]);
+
+  if (checkingAuth) return <Loading />;
+  return <>{children}</>;
 };
 
 export default PowerUserRoute;
