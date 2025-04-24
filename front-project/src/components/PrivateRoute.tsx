33ed1ffb7446c59c3ebd38cb83f7912cf75b75a0
ounjuu <koj1555@naver.com>
2025-04-24 12:23:42 +0900

    Fix : privateRoute 수정중

--- front-project/src/components/PrivateRoute.tsx
+++ front-project/src/components/PrivateRoute.tsx
@@ -9,24 +9,46 @@ interface PrivateRouteProps {
 }
 
 const PrivateRoute = ({ children }: PrivateRouteProps) => {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, checkLogin } = useAuth();
   const router = useRouter();
   const [checkingAuth, setCheckingAuth] = useState(true); // 3초 동안 로딩 상태
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setCheckingAuth(false);
-      if (!isLoggedIn) {
-        alert("로그인이 필요합니다.");
-        router.push("/login");
-      }
-    }, 500);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setCheckingAuth(false);
+  //     if (!isLoggedIn) {
+  //       alert("로그인이 필요합니다.");
+  //       router.push("/login");
+  //     }
+  //   }, 500);
 
-    return () => clearTimeout(timer);
-  }, [isLoggedIn, router]);
+  //   return () => clearTimeout(timer);
+  // }, [isLoggedIn, router]);
+
+  // if (!isLoggedIn) {
+  //   return <Loading />; // 로딩 중일때 로딩 중 페이지 띄우기
+  // }
+
+  // return <>{children}</>; // 로그인된 경우 자식 컴포넌트 렌더링
+
+  useEffect(() => {
+    // checkLogin 함수가 로그인 상태를 확인하도록 호출
+    const checkUserAuth = async () => {
+      await checkLogin(); // 인증 상태 확인
+      setCheckingAuth(false); // 인증 상태 확인 완료
+    };
+
+    checkUserAuth();
+  }, [checkLogin]);
+
+  if (checkingAuth) {
+    return <Loading />; // 인증 상태 확인 중에는 로딩 화면 표시
+  }
 
   if (!isLoggedIn) {
-    return <Loading />; // 로딩 중일때 로딩 중 페이지 띄우기
+    alert("로그인이 필요합니다.");
+    router.push("/login");
+    return null; // 로그인되지 않은 경우 렌더링 중지
   }
 
   return <>{children}</>; // 로그인된 경우 자식 컴포넌트 렌더링
